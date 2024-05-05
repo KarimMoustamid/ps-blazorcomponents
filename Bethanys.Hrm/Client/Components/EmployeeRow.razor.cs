0c9f1fb6909f1886eb8d47538931f5817bd88841
karimmoustamid <moustamid.teck@gmail.com>
2024-05-05 10:15:27 -0600

    Add EmployeeRow component and update EmployeeOverview page
    Created a new EmployeeRow component to display employee information in a structured way. The EmployeeOverview page has been updated to utilize this component. This change promotes better code organization and reusability.

---
+++ Bethanys.Hrm/Client/Components/EmployeeRow.razor.cs
@@ -0,0 +1,11 @@
+namespace Bethanys.Hrm.Client.Components
+{
+    using Hrm.Shared;
+    using Microsoft.AspNetCore.Components;
+
+    public partial class EmployeeRow
+    {
+        [Parameter]
+        public EmployeeModel Employee { get; set; }
+    }
+}
